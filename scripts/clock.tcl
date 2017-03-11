fbcca69131763665f9b04d4e9ba534e37e872249
Sean Zarzycki <seanzarz@gmail.com>
2017-03-10 23:21:04 -0500

    scripts added

---
+++ scripts/clock.tcl
@@ -0,0 +1,7 @@
+create_clock gclk -period 2 
+#set_fix_hold [find clock gclk]
+set_clock_transition 0.1 gclk
+set_clock_latency 0.2 gclk
+set_timing_derate -data 0.1
+set_timing_derate -clock 0.1
+set_wire_load_mode enclosed
